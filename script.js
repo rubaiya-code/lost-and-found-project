86c2f407ce1b715811e8db41765dd7e41ece7137
Mafy-Ahammed <mafyahammed786@gmail.com>
2025-08-19 04:06:11 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Default item count
+let itemCount = 0;
+
+// Show count in page
+document.getElementById("itemCount").textContent = itemCount;
+
+// Just for fun animation
+console.log("Lost & Found Website Loaded Successfully 🎉");
